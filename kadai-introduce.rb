e88566f53ae54c5e32fde0f882af7730a8d97344
tadafumi.oka <tadafumi_app@gmail.com>
2022-11-28 10:53:24 +0000

    kadai-ruby-1

---
+++ kadai-introduce.rb
@@ -0,0 +1,11 @@
+# 下記は私の名前を表示するプログラムです
+myoji = "岡"
+namae = "理史"
+
+# 下記は私の年齢を表示するプログラムです
+kotoshi = 2022
+umaretatoshi = 1978
+num = kotoshi - umaretatoshi
+str = "歳です。"
+
+puts myoji + namae + "です。" + num.to_s + str
